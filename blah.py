d8cb4f24992567752018b7a9364e745a911f2543
jocelorrey <jocelorrey@gmail.com>
2021-06-30 23:37:41 -0400

    Plots points w/o map in background

--- blah.py
+++ blah.py
@@ -8,36 +8,46 @@ import matplotlib.pyplot as plt
 # Boston Transportation Districts Shapefile:
 # https://data.boston.gov/dataset/boston-transportation-department-btd-districts1/resource/fe16d9d0-92ff-4f71-b021-9cceffba47ed
 
-# Load data from CSV into a oandas dataframe
+# Load data from CSV into a pandas dataframe
 data_file = "./data/Wicked_Free_WiFi_Locations.csv"
-columns_of_interest = ['device_lat', 'device_long', 'neighborhood_name']
+columns_of_interest = ['device_lat', 'device_long'] #, 'neighborhood_name']
 wifi_location_df = pd.read_csv(data_file, usecols=columns_of_interest)
 
-# Load the shapefile into a geopandas dataframe
+# specify lat/long geometry
+geometry = gpd.points_from_xy(wifi_location_df.device_lat, 
+                              wifi_location_df.device_long)
+
+
+wifi_location_df['coords'] = wifi_location_df[['device_long', 'device_lat']].values.tolist()
+wifi_location_df['coords'] = wifi_location_df['coords'].apply(Point)
+wifi_location_df
+
+# Load the Boston shapefile into a geopandas dataframe
 boston_map = gpd.read_file('./data/Boston_Transportation_Department_(BTD)_Districts_.shp')
 
-# Unclear why this is needed:
-#geometry = [Point(xy) for xy in zip(wifi_location_df['device_long'], wifi_location_df['device_lat'])]
-# create GeoPandas dataframe
-my_crs = boston_map.crs
-print(my_crs)
-geometry = gpd.points_from_xy(wifi_location_df.device_lat, wifi_location_df.device_long)
-geo_df = gpd.GeoDataFrame(wifi_location_df, crs=my_crs, geometry=geometry)
+# Create geopandas dataframe for wifi data
+geo_df = gpd.GeoDataFrame(wifi_location_df, 
+                          crs=boston_map.crs, 
+                          geometry='coords')
+
+# Ensure the coordinate reference systems are the same
+print("Wifi Locations:", geo_df.crs)
+print("Boston Map:", boston_map.crs) # both epsg:2249 (MA mainland)
 
 #print(geo_df)
 
 # Plot it
 fig, ax = plt.subplots(figsize=(8, 5))
 
-boston_map.plot(ax=ax, alpha=0.2, color="blue")
+boston_map.plot(ax=ax, alpha=0.4, color="blue")
 geo_df.plot(ax=ax, alpha=0.9, color="red")
-
+print(geo_df)
 plt.title("Boston's Wicked Free Wifi Locations")
 
-#plt.xlim(70.985278, 71.191306) # x = longitude
-#plt.ylim(42.225861, 42.398000) # y = latitude
+plt.xlim(-71.191306, -70.985278) # x = longitude
+plt.ylim(42.225861, 42.398000) # y = latitude
 
-#plt.show()
+plt.show()
 # Credits to 
 # 1. https://towardsdatascience.com/how-to-read-csv-file-using-pandas-ab1f5e7e7b58
 # 2. https://stackoverflow.com/questions/53233228/plot-latitude-longitude-from-csv-in-python-3-6
